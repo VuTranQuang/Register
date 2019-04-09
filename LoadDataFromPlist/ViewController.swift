553e3a1929e19704887cfc513e82990c4e9e8473
Vu <vutran7993@gmail.com>
2019-04-10 06:50:14 +0700

    Initial Commit

---
+++ LoadDataFromPlist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoadDataFromPlist
+//
+//  Created by Vu on 4/10/19.
+//  Copyright © 2019 Vu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
